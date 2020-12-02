af45bafa61d7e1352bcf2fd7388b5ff460f667e4
LukasK13 <spam.lukas.klass@gmail.com>
2020-12-02 09:24:08 +0100

    Use RESTful API

--- check_home-assistant.py
+++ check_home-assistant.py
@@ -1,55 +1,34 @@
 #!/usr/bin/python3
 
-import asyncio
-import json
-import asyncws
+import requests as req
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(prog="check_mailcow.py", description='Check Home-Assistant API for state')
+parser = argparse.ArgumentParser(prog="check_home-assistant.py", description='Check Home-Assistant API for state')
 parser.add_argument("-u", "--url", dest='url', default="localhost:8123",
                     metavar="localhost:8123", help="URL to the Home-Assistant API.")
 parser.add_argument("-t", "--token", dest="token", metavar="Access-Token",
                     help="Long live access token for Home-Assistant.", required=True)
+parser.add_argument("-s", "--ssl", dest="ssl", default=False, action='store_true', help="Use SSL for the connection.")
+parser.add_argument("-i", "--insecure", dest="verify", default=True, action='store_false',
+                    help="Insecure connection. Don't verify the SSL certificate.")
 args = parser.parse_args()
 
+headers = {"Authorization": "Bearer " + args.token, "content-type": "application/json"}
+try:
+    res = req.get(("https" if args.ssl else "http") + "://" + args.url + "/api", headers=headers, verify=args.verify)
+except:
+    print("Unknown: Connection refused.")
+    sys.exit(3)
 
-async def main():
-    """Simple WebSocket client for Home Assistant."""
-    websocket = await asyncws.connect("ws://" + args.url + "/api/websocket")
-    await websocket.recv()
-
-    await websocket.send(json.dumps(
-        {'type': 'auth',
-         'access_token': args.token}
-    ))
-    await websocket.recv()
-
-    await websocket.send(json.dumps(
-        {'id': 1, 'type': 'system_health/info'}
-    ))
-
-    message = await websocket.recv()
-    message = json.loads(message)
-    if message is not None:
-        if "result" in message.keys():
-            if "homeassistant" in message['result'].keys():
-                if "version" in message['result']['homeassistant'].keys():
-                    print("OK: Home-Assistant version " + message['result']['homeassistant']['version'])
-                else:
-                    print("Critical: missing key 'version'.")
-                    sys.exit(2)
-            else:
-                print("Critical: missing key 'homeassistant'.")
-                sys.exit(2)
-        else:
-            print("Critical: missing key 'result'.")
-            sys.exit(2)
+if res.status_code == 200:
+    state_mes = res.json()
+    state = state_mes["message"]
+    if state == "API running.":
+        print("OK: API is in state 'running'.")
     else:
-        print("Critical: got no response.")
+        print("Critical: API is in state '" + state + "'")
         sys.exit(2)
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-loop.close()
+else:
+    print("Unknown: Request returned status code " + str(res.status_code) + ".")
+    sys.exit(3)
